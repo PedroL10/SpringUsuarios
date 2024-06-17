20336ab525b3fd9ace94821ddd91371ffe2c47e2
Pedro Goncalves <pepluisgon01@gmail.com>
2024-06-17 15:26:30 -0300

    Linkando STS

--- src/main/java/br/com/pedrolg/projeto/ProjetoBackendApplication.java
+++ src/main/java/br/com/pedrolg/projeto/ProjetoBackendApplication.java
@@ -3,6 +3,8 @@ package br.com.pedrolg.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Linkando git no STS
+
 @SpringBootApplication
 public class ProjetoBackendApplication {
 
