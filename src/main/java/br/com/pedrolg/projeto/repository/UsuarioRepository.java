b3c27a81e92e3e315dc20284595aff755e6f1ae9
Pedro Goncalves <pepluisgon01@gmail.com>
2024-08-14 16:42:47 -0300

    Parte da autenticação

--- src/main/java/br/com/pedrolg/projeto/repository/UsuarioRepository.java
+++ src/main/java/br/com/pedrolg/projeto/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.pedrolg.projeto.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.pedrolg.projeto.entity.UsuarioEntity;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+	
+	Optional<UsuarioEntity> findByLogin(String login);
 
 }
